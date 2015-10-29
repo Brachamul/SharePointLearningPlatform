98180b44043400b7c4466dee38764b7f7cccc676
Brachamul <barnaby.brachamul@gmail.com>
2015-10-29 18:15:34 +0100

    !

--- custom_solucom.js
+++ custom_solucom.js
@@ -4,26 +4,56 @@ jQuery(function() {
 	$('#notificationArea').hide()
 
 	// Contenu des onglets
-	$('[id*="Ribbon"][role="tabpanel"]').hide()
-
 	$('.ms-cui-tabContainer').hide()
 
 	// Onglets
-	$('[id*="Ribbon"].ms-cui-tt').click(function(){
+	$('.ms-cui-tt[data-tab]').click(function(){
+		var tab = $(this).attr('data-tab')
+		console.log(tab)
 		if ($(this).hasClass('ms-cui-tt-s')) {
 			$(this).removeClass('ms-cui-tt-s')
 			$('.ms-cui-tabContainer').slideUp('fast')
 		} else {
+			$('.ms-cui-tabContainer:not([data-ribbon="' + tab + '"])').slideUp('fast')
 			$('.ms-cui-tt-s').removeClass('ms-cui-tt-s')
 			$(this).addClass('ms-cui-tt-s')
-			$('[role="tabpanel"]').hide()
-			$('.ms-cui-tabContainer').hide()
-			id = $(this).attr("id").replace("-title", "")
-			$('[id*="' + id + '"][role="tabpanel"]').show()
-			$('.ms-cui-tabContainer').slideDown('fast')
+			$('[data-ribbon="' + tab + '"]').slideDown('fast')
 		}
 	})
 
+	$('.no-clicky').click(function(){
+		var response = "Mais pourquoi donc avez-vous cliqué ici ?"
+		var context = $(this).attr("data-noclicky")
+		if (context) response = context
+		openModal(response)
+		return false
+	})
 
+	$('.o365cs-nav-contextMenu').hide()
+	$('#O365_MainLink_Settings').click(function(){
+		$('.o365cs-nav-contextMenu').slideToggle('fast')
+	})
+
+	$('.edit-page').click(function() {
+		alert("Go mode édition")
+		return false
+	})
+
+	$('body').prepend('<div id="solucom-modal"><div class="container"><div class="subcontainer"><img src="shupert.png"/><div class="content"></div></div></div></div>')
+
+	function openModal(text) {
+		$('#solucom-modal .content').html(text)
+		$('#solucom-modal').fadeIn('fast')
+	}
+
+	$('#solucom-modal').click( function() {
+		$(this).fadeOut('fast')
+	})
+
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) $('#solucom-modal').fadeOut('fast')
+	});
+
+	$('#DeltaPlaceHolderMain').load('content_accueil.html')
 
 });
